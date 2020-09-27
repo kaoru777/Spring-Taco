729cfba5352bedd723a596a830be97379bb2b435
Kaoru Steinboeck <kaoru@steinboeck.dev>
2020-09-27 15:51:39 +0200

    added: chapter 2 adapted solution

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -2,6 +2,7 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -10,4 +11,7 @@ public class TacoCloudApplication {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 }
